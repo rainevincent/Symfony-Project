876c2873b7823602a9b8166a1eee3c685903b74b
rainevincent <raine_v@vrwd-freelancer.eu>
2016-01-22 02:00:22 +0100

    Update template index.html.twig

--- src/WC/PlatformBundle/Controller/AdvertController.php
+++ src/WC/PlatformBundle/Controller/AdvertController.php
@@ -2,16 +2,18 @@
 /**
  * @Author: raine_v
  * @Date:   2016-01-21 22:57:11
- * @Last Modified by:   raine_v
- * @Last Modified time: 2016-01-21 23:06:44
+ * @Last Modified by:   rainevincent
+ * @Last Modified time: 2016-01-22 01:09:35
  */
  
  // src/WC/PlatformBundle/Controller/AdvertController.php
  
  namespace WC\PlatformBundle\Controller;
+ 
+ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
  use Symfony\Component\HttpFoundation\Response;
 
- class AdvertController 
+ class AdvertController extends Controller
  {
  	/**
  	 * Display content page resquest
@@ -21,7 +23,8 @@
  	 */
  	public function indexAction()
  	{
- 		return new Response("Hello World !");
+ 		$content = $this->get("templating")->render("WCPlatformBundle:Advert:index.html.twig");
+ 		return new Response($content);
  	}
  }
  
